da8fa69092004b6f97cc237e83724d221e3a75fb
hermsale <hermsale@gmail.com>
2024-11-06 19:44:32 -0300

    se siguio trabajando en los metodos

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,13 +3,33 @@ public class Cliente extends Persona{
     // atributos exclusivos de Cliente
     private String telefono;
     private String direccion;
-
-
+    
     public Cliente(String nombre, String apellido, int edad, String email, String telefono, String direccion) {
         super(nombre, apellido, edad, email);
         this.telefono = telefono;
         this.direccion = direccion;
     }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+
 
 
 }
